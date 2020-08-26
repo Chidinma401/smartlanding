4a75b288618bfbd5dba6d37e9949a8f94af10b93
Chidinma Ike <chidinmaike6@gmail.com>
2020-08-26 10:48:54 +0100

    landing page by me

---
+++ js/main.js
@@ -0,0 +1,128 @@
+function sayWelcome(){
+	alert("welcome boy");
+}
+
+var a =5;
+var b=10;
+var c=a+b;
+console.log(c);
+
+var a=10;
+var b=10;
+var c=a-(b);
+console.log(c);
+
+var a=10;
+var b=4;
+var c=a*(b);
+console.log(c);
+
+var a=3;
+var b=6;
+var c=a/b;
+console.log(c);
+
+var a=5;
+var b=8;
+var c=a%b ;
+console.log(c);
+
+var d=10;
+var e=5;
+console.log(d+e);
+
+var d=10;
+var e=5;
+console.log(d-e);
+
+var d=10;
+var e=5;
+console.log(d*e);
+
+var d=10;
+var e=5;
+console.log("the sum is:"+(d/=e));
+
+var g=2;
+var h=2
+console.log(2==2)
+ v="5";
+ x=3;
+ console.log(v=x);
+ var name="solomon";
+ console.log("my name is "+ name);
+
+var daniel ="boy";
+
+if(daniel =="boy"){
+	console.log('make her hair');
+}
+else { 
+	console.log("cut his hair");
+}
+
+var amdin='hugry'
+
+if(amdin =='hungry'){
+	console.log( "give her food");
+}
+else{
+	console.log("done give her food");
+}
+
+var age=20;
+var notice;
+if(age >=21 && age<=24 ){
+	notice="come in to the party";
+
+}
+else if(age==20){
+	notice= "we will just consider you"; 
+}
+
+	else if (age == 19){
+		notice= "sorry, this is not for you";
+	}
+
+else{
+	notice="what is your age please";
+}
+console.log(notice);
+
+var red=40;
+var notice;
+if( red >=30 && red<=40){
+	notice="done wear red to the party";
+}
+else if(red==29){
+	notice="we will consider you";
+}
+else if(red==20){
+	notice="is not for you";
+}
+else{
+	notice="what is your color";
+}
+console.log(notice);
+
+var green=30;
+var notice;
+if( "green" >=30 && "best color"<=40){
+	notice="red or green";
+}
+else if(red==29){
+	notice="if red will consider you";
+}
+else if(red==20){
+	notice="is not for green";
+}
+else{
+	notice="what is your best color";
+}
+console.log(notice);
+
+
+
+
+	
+
